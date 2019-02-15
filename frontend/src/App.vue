ebb0d7ec5fb5fc98187c50784041835ce7555bf5
hayden7913 <hayden321@gmail.com>
2019-02-14 23:36:00 -0800

    Removes bottom nav from App component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,21 +4,13 @@
       <div class="app-main">
         <router-view />
       </div>
-      <div class="app-nav">
-        <BaseNav />
-      </div>
     </div>
   </v-app>
 </template>
 
 <script>
-import BaseNav from '@/components/BaseNav';
-
 export default {
   name: 'App',
-  components: {
-    BaseNav,
-  },
 };
 </script>
 
@@ -26,24 +18,5 @@ export default {
 // overrides vuetify's default background
 .theme--light.application {
   background: white;
-}
-
-.app-container {
-  display: grid;
-  height: 100%;
-  width: 100%;
-  grid-template-areas:
-    "main"
-    "nav";
-  grid-template-rows: 5fr 1fr;
-
-  .app-main {
-    grid-area: main;
-    border: 1px solid tomato;
-  }
-
-  .app-nav {
-    grid-area: nav;
-  }
 }
 </style>
