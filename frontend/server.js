88403ec22ba79e064cf80ceacc8253dbb8f380c8
hayden7913 <hayden321@gmail.com>
2019-02-27 16:30:31 -0800

    adds express server to serve dist folder

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('dist'));
