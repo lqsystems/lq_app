9a1e4cb549843e9ed67d9a914b85f59613957cea
hayden7913 <hayden321@gmail.com>
2019-04-03 00:08:25 -0700

    adds sidebar header component

---
+++ frontend/src/components/BaseSidebarHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="sb-header">
+    <img
+      class="sb-header-icon"
+      src="@/assets/reactor-3d-light.png"
+      alt="reactor icon"
+    >
+    <div class="sb-header-title">
+      Zee Prime
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSidebarHeader',
+};
+
+</script>
+
+<style lang='scss' scoped>
+.sb-header {
+  display: flex;
+  align-items: center;
+  padding: 1em 1.3em;
+}
+
+.sb-header-icon {
+  width: 2.4em;
+  margin-right: 1em;
+}
+
+.sb-header-title {
+  font-size: 1.6em;
+}
+
+</style>
