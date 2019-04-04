65d1076b0b76d614035fd02cafe68bdd416b1f10
hayden7913 <hayden321@gmail.com>
2019-04-03 17:07:23 -0700

    add reactor card to reactor index view

---
+++ frontend/src/components/ReactorIndexCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <BaseCard class="reactor-card">
+    <div class="reactor-card-data">
+      <div class="reactor-card-temp">
+        <span class="reactor-card-temp-val">31.3</span>
+        <span class="reactor-card-data-unit reactor-card-temp-unit">°C</span>
+      </div>
+      <div class="divider-horiz" />
+      <div class="reactor-card-od">
+        <span class="reactor-card-od-val">3.67</span>
+        <span class="reactor-card-data-unit reactor-card-od-unit">OD</span>
+      </div>
+    </div>
+    <div class="reactor-card-title">
+      MV1
+    </div>
+  </BaseCard>
+</template>
+
+<script>
+import BaseCard from '@/components/BaseCard';
+
+export default {
+  name: 'ReactorIndexCard',
+  components: {
+    BaseCard,
+  },
+};
+</script>
+<style lang='scss' scoped>
+@import '../styles/variables';
+$reactor-card-accent-color: lightseagreen;
+
+.reactor-card {
+  background-color: $color-panel-background;
+  display: inline-block;
+  padding: 1.5em 2em;
+  border-bottom: 3px solid $reactor-card-accent-color;
+}
+
+.reactor-card-data {
+  display: flex;
+  justify-content: center;
+  font-size: 2.5em;
+  margin-bottom: 3em;
+}
+
+.reactor-card-data-unit {
+  vertical-align: super;
+  font-size: .5em;
+}
+
+.reactor-card-title {
+  font-size: 1.9em;
+}
+
+.divider-horiz {
+  border-right: 2px solid $reactor-card-accent-color;
+  margin: 0 1em;
+}
+</style>
--- frontend/src/views/ReactorIndex.vue
+++ frontend/src/views/ReactorIndex.vue
@@ -1,73 +1,29 @@
 <template>
   <div class="reactor-index">
-    <div class="reactor-card">
-      1.46C
+    <div class="reactor-card-wrapper">
+      <ReactorIndexCard />
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-import { FETCH_MODULES } from '@/store/actions.types.js';
-import { LOGIN_URL } from '@/constants/api.constants.js';
-import callApi from '@/utils/api.utils.js';
+import ReactorIndexCard from '@/components/ReactorIndexCard';
 
 export default {
-  name: 'Login',
-  data() {
-    return {
-      username: '',
-      password: '',
-    };
-  },
-  methods: {
-    ...mapActions([FETCH_MODULES]),
-    async login() {
-      const credentials = {
-        username: this.username,
-        password: this.password,
-      };
-
-      const { data: error } = await callApi(LOGIN_URL, {
-        method: 'POST',
-        data: credentials,
-      });
-
-      if (error.message && error.message[0]) {
-        return alert(error.message[0]);
-      }
-
-      this.FETCH_MODULES('/');
-    },
+  name: 'ReactorIndex',
+  components: {
+    ReactorIndexCard,
   },
 };
-
 </script>
 
 <style scoped lang="scss">
-.login-wrapper {
-  width: 550px;
-  margin: 15vh auto;
+.reactor-index {
+  color: lightgrey;
 }
 
-.v-card {
-  padding: 1.5em;
-}
-
-.lq-title {
-  padding-top: 1em;
-}
-
-.lq-title-text {
-  font-size: 1.4em;
-  padding-top: 0;
-}
-.lq-title-text {
-  font-size: 1.5em;
-  padding-top: 0;
-}
-.lq-title-text {
-  font-size: 1.5em;
-  padding-top: 0;
+.reactor-card-wrapper {
+  width: 50%;
+  margin: 20% auto;
 }
 </style>
