03e27cfecaeea02b3478e78a98cc02e655043fd1
hayden7913 <hayden321@gmail.com>
2019-08-31 21:16:31 -0700

    removes conditional render

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
-
 <template>
-  <v-app v-if="!isFetching">
+  <v-app>
     <transition
       name="router-animation"
       enter-active-class="animated fadeIn"
@@ -12,7 +11,8 @@
 </template>
 
 <script>
-import { mapActions, mapMutations, mapGetters } from 'vuex';
+
+import { mapActions, mapMutations } from 'vuex';
 import { diff } from 'deep-object-diff';
 import { diffStatesOnUpdateMessage, getModuleByReactionId } from '@/utils/entities.utils';
 import { FETCH_MODULES, HANDLE_UPDATE_STATE_MESSAGE } from '@/store/actions.types';
@@ -20,9 +20,6 @@ import { MUTATE_MODULE_STATE, SOCKET_DATUM } from '@/store/mutations.types';
 
 export default {
   name: 'App',
-  computed: {
-    ...mapGetters(['isFetching']),
-  },
   created() {
     this.FETCH_MODULES();
   },
