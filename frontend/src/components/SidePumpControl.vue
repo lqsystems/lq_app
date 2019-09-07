6f926bed3e5efe653650f36618ea0f9a972bf74c
hayden7913 <hayden321@gmail.com>
2019-09-06 19:21:32 -0700

    updates pump state on switch toggle

--- frontend/src/components/SidePumpControl.vue
+++ frontend/src/components/SidePumpControl.vue
@@ -2,22 +2,23 @@
   <div>
     <div>Load</div>
     <SwitchControl
-      :is-on="activeModuleState.water"
-      :is-disabled="activeModuleState.extraction"
+      :is-on="mv1State.water"
+      :is-disabled="mv1State.extraction"
       @toggle="toggleLoad"
     />
     <div>Extract</div>
     <SwitchControl
-      :is-on="activeModuleState.extraction"
-      :is-disabled="activeModuleState.water"
+      :is-on="mv1State.extraction"
+      :is-disabled="mv1State.water"
       @toggle="toggleExtraction"
     />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions, mapMutations } from 'vuex';
 import { UPDATE_MODULE_STATE } from '@/store/actions.types';
+import { UPDATE_SELECTED_MODULE } from '@/store/mutations.types';
 import SwitchControl from './SwitchControl';
 
 export default {
@@ -26,22 +27,23 @@ export default {
     SwitchControl,
   },
   computed: {
-    ...mapGetters(['activeModuleState', 'mv1State', 'getApiUpdatePayload']),
+    ...mapGetters(['mv1State', 'selectedModuleName']),
   },
   created() {
     this.toggleLoad = this.togglePump('water');
     this.toggleExtraction = this.togglePump('extraction');
   },
-  mounted() {
-    console.log(this.getApiUpdatePayload);
-  },
   methods: {
+    ...mapMutations([UPDATE_SELECTED_MODULE]),
     ...mapActions([UPDATE_MODULE_STATE]),
     updateState(actuatorType, newState) {
+      const prevSelected = this.selectedModuleName;
+      this.UPDATE_SELECTED_MODULE('MV1');
       this.UPDATE_MODULE_STATE({
         actuatorType,
         newState,
       });
+      this.UPDATE_SELECTED_MODULE(prevSelected);
     },
     togglePump(pumpActionType) {
       return (newState) => {
