8a2823747f6b5ff2bd609d0bef5802bab508a8ba
hayden7913 <hayden321@gmail.com>
2019-09-14 14:00:15 -0700

    build

--- backend/dist/precache-manifest.0a22e9418947b6136aca91b43ad7bfbc.js
+++ backend/dist/precache-manifest.0a22e9418947b6136aca91b43ad7bfbc.js
@@ -1,75 +1,67 @@
 self.__precacheManifest = [
   {
-    "revision": "2757e2af00f1bd1bfc83",
+    "revision": "0c7e86509951663a486d",
     "url": "/vue/css/app.3f81e402.css"
   },
   {
-    "revision": "2757e2af00f1bd1bfc83",
-    "url": "/vue/js/app.10801174.js"
+    "revision": "0c7e86509951663a486d",
+    "url": "/vue/js/app.90efcea3.js"
   },
   {
-    "revision": "70e8d1b72a5c076e1a4a",
-    "url": "/vue/css/chunk-4c306375.6e58c4f6.css"
+    "revision": "209f3d3a07237faf5dab",
+    "url": "/vue/css/chunk-1472113f.4c4df56e.css"
   },
   {
-    "revision": "70e8d1b72a5c076e1a4a",
-    "url": "/vue/js/chunk-4c306375.deabebc6.js"
+    "revision": "209f3d3a07237faf5dab",
+    "url": "/vue/js/chunk-1472113f.d03db923.js"
   },
   {
-    "revision": "f9ce66883fa5fc5087ec",
-    "url": "/vue/css/chunk-57f8977a.18cd2634.css"
+    "revision": "7201db6d27fcdeb1b672",
+    "url": "/vue/css/chunk-1a7ea569.30e3e8b8.css"
   },
   {
-    "revision": "f9ce66883fa5fc5087ec",
-    "url": "/vue/js/chunk-57f8977a.36cd68d2.js"
+    "revision": "7201db6d27fcdeb1b672",
+    "url": "/vue/js/chunk-1a7ea569.f58e193d.js"
   },
   {
-    "revision": "08c56ffaf7e5c3e8f3ce",
+    "revision": "e87e84698f592c8048bc",
+    "url": "/vue/css/chunk-4c1123b8.61260c67.css"
+  },
+  {
+    "revision": "e87e84698f592c8048bc",
+    "url": "/vue/js/chunk-4c1123b8.47da45d1.js"
+  },
+  {
+    "revision": "2b47d8d5b7f947d05342",
     "url": "/vue/css/chunk-5878c818.becf85cd.css"
   },
   {
-    "revision": "08c56ffaf7e5c3e8f3ce",
-    "url": "/vue/js/chunk-5878c818.7773da10.js"
+    "revision": "2b47d8d5b7f947d05342",
+    "url": "/vue/js/chunk-5878c818.896a6cf2.js"
   },
   {
-    "revision": "99434d292ce4fac44a1c",
+    "revision": "a68134ea72a1b001e026",
     "url": "/vue/css/chunk-5f443b6e.f7442519.css"
   },
   {
-    "revision": "99434d292ce4fac44a1c",
-    "url": "/vue/js/chunk-5f443b6e.8de20245.js"
+    "revision": "a68134ea72a1b001e026",
+    "url": "/vue/js/chunk-5f443b6e.e7119cd0.js"
   },
   {
-    "revision": "f3b14fe48fb81bc0b70d",
+    "revision": "27fee99aab0481330e3d",
     "url": "/vue/css/chunk-77b3c4ba.10c1110f.css"
   },
   {
-    "revision": "f3b14fe48fb81bc0b70d",
-    "url": "/vue/js/chunk-77b3c4ba.413bcc73.js"
+    "revision": "27fee99aab0481330e3d",
+    "url": "/vue/js/chunk-77b3c4ba.79465f5f.js"
   },
   {
-    "revision": "3e9bb370fc44ad8fe33c",
-    "url": "/vue/css/chunk-7958704e.9bd84504.css"
-  },
-  {
-    "revision": "3e9bb370fc44ad8fe33c",
-    "url": "/vue/js/chunk-7958704e.4930f98f.js"
-  },
-  {
-    "revision": "480d60b767bf00a0fbb8",
-    "url": "/vue/css/chunk-b2a0fd02.473b4703.css"
-  },
-  {
-    "revision": "480d60b767bf00a0fbb8",
-    "url": "/vue/js/chunk-b2a0fd02.861f480a.js"
-  },
-  {
-    "revision": "1adbd29e385f958ae83a",
+    "revision": "c7c309a7e4d1a0c54eea",
     "url": "/vue/css/chunk-vendors.4384cebb.css"
   },
   {
-    "revision": "1adbd29e385f958ae83a",
-    "url": "/vue/js/chunk-vendors.14622935.js"
+    "revision": "c7c309a7e4d1a0c54eea",
+    "url": "/vue/js/chunk-vendors.73886d07.js"
   },
   {
     "revision": "fd27aec9cd6f7f619a21f5d5bfe3b5ff",
@@ -88,6 +80,10 @@ self.__precacheManifest = [
     "url": "/vue/img/kapsel-logo.c043d58e.png"
   },
   {
+    "revision": "160b9cd7d155ec08bd4427dc87afb3ef",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-300.160b9cd7.eot"
+  },
+  {
     "revision": "4f5f5c645bf1a0cd30d0927f363da4a8",
     "url": "/vue/fonts/roboto-condensed-v17-latin-300.4f5f5c64.woff2"
   },
@@ -96,16 +92,8 @@ self.__precacheManifest = [
     "url": "/vue/fonts/roboto-condensed-v17-latin-300.b8f4e373.woff"
   },
   {
-    "revision": "160b9cd7d155ec08bd4427dc87afb3ef",
-    "url": "/vue/fonts/roboto-condensed-v17-latin-300.160b9cd7.eot"
-  },
-  {
     "revision": "f87018195ced0dad8d4e3e6562c80516",
     "url": "/vue/fonts/roboto-condensed-v17-latin-300.f8701819.ttf"
-  },
-  {
-    "revision": "4230f241836bcfeebad3db578e0c944e",
-    "url": "/vue/fonts/roboto-condensed-v17-latin-regular.4230f241.eot"
   },
   {
     "revision": "3ada4464a5d4d208c5e3591076c5138a",
@@ -114,6 +102,10 @@ self.__precacheManifest = [
   {
     "revision": "5c7a07a5cc1fd3d8196e6f2a2e0f76ff",
     "url": "/vue/fonts/roboto-condensed-v17-latin-regular.5c7a07a5.woff"
+  },
+  {
+    "revision": "4230f241836bcfeebad3db578e0c944e",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-regular.4230f241.eot"
   },
   {
     "revision": "b47c5c18180aeaa8de60282ab7667d6f",
@@ -136,12 +128,12 @@ self.__precacheManifest = [
     "url": "/vue/fonts/roboto-condensed-v17-latin-700.8b5d9f3f.ttf"
   },
   {
-    "revision": "f3ae85bcdd2230f15b5127256b5c97c0",
-    "url": "/vue/img/roboto-condensed-v17-latin-300.f3ae85bc.svg"
-  },
-  {
     "revision": "8cb983f9db2dc3cf43e7879513126a5c",
     "url": "/vue/img/roboto-condensed-v17-latin-regular.8cb983f9.svg"
+  },
+  {
+    "revision": "f3ae85bcdd2230f15b5127256b5c97c0",
+    "url": "/vue/img/roboto-condensed-v17-latin-300.f3ae85bc.svg"
   },
   {
     "revision": "587ed44908df84877c5a7f668eb1dfa1",
@@ -152,12 +144,12 @@ self.__precacheManifest = [
     "url": "/vue/img/icomoon.dddfbd08.svg"
   },
   {
-    "revision": "570eb83859dc23dd0eec423a49e147fe",
-    "url": "/vue/fonts/MaterialIcons-Regular.570eb838.woff2"
-  },
-  {
     "revision": "e79bfd88537def476913f3ed52f4f4b3",
     "url": "/vue/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/vue/fonts/MaterialIcons-Regular.570eb838.woff2"
   },
   {
     "revision": "012cf6a10129e2275d79d6adac7f3b02",
@@ -168,7 +160,7 @@ self.__precacheManifest = [
     "url": "/vue/fonts/MaterialIcons-Regular.a37b0c01.ttf"
   },
   {
-    "revision": "be40f6b20c7449fe7ee4bfe17c8680fe",
+    "revision": "63ea4c67ee80c079a57ded8548fc3d5d",
     "url": "/vue/index.html"
   },
   {
